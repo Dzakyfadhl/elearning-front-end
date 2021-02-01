32293bed722d4cf46cbd3d5836560547c880d328
farrelyudap <farrelyudapraditya96@gmail.com>
2021-02-01 09:39:06 +0700

    Update home teacher, responsive layout attendences and submission

--- projects/lawerning/src/app/page/teacher/home-teacher/home-teacher.component.ts
+++ projects/lawerning/src/app/page/teacher/home-teacher/home-teacher.component.ts
@@ -7,10 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeTeacherComponent implements OnInit {
 
+  courses: {
+    title: string
+    startDate: string,
+    endDate: string,
+    description: string
+  }[]
   constructor() {
+    
   }
 
   ngOnInit(): void {
-    
+    this.courses =
+    [
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      },
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      },
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      },
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      },
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      },
+      {
+        title: "Object Oriented Programming",
+        startDate: "21 Jan 2021",
+        endDate: "28 Jan 2021",
+        description: `Belajar programming menggunakan bahasa C 
+      yang menjadi populer dikarenakan cepat dan 
+      mudah untuk dipelajari bagi para pemula.`
+      }
+    ]
   }
 }
